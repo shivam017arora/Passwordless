4f6deddf9502bb7ecf3babdb9d2d77b7f23985c1
Shivam017Arora <shivam017aroar@gmail.com>
2023-06-24 16:41:02 -0700

    Closes #2

---
+++ frontend/app/(default)/page.tsx
@@ -0,0 +1,19 @@
+export const metadata = {
+  title: 'Home - Simple',
+  description: 'Page description',
+}
+
+import Hero from '@/components/hero'
+import Features from '@/components/features'
+import Testimonials from '@/components/testimonials'
+import Newsletter from '@/components/newsletter'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Testimonials />
+    </>
+  )
+}
